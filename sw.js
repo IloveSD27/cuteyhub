b75dbdbc3571768a49abed324aff13190c76a4b1
IloveSD27 <anna.koepfer@gmail.com>
2026-04-02 17:23:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -38,15 +38,18 @@ self.addEventListener('notificationclick', e => {
   );
 });
 
-const CACHE = 'cuteyhub-v2';
+const CACHE = 'cuteyhub-v3';
+
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html', '/icon.png', '/manifest.json'])));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
